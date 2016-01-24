49196cbe97e429a0977f0707558eb3c50c90e1a1
JasonMarkWomack <jwomack155@gmail.com>
2016-01-24 12:19:13 -0500

    ruby

---
+++ script.rb
@@ -0,0 +1,19 @@
+print "What's your first name?"
+first_name = gets.chomp.capitalize!
+first_name = "Jason"
+puts "Your name is #{first_name}!"
+
+print "What's your last name?"
+last_name = gets.chomp.capitalize!
+last_name = "Womack"
+puts "Your name is #{last_name}!"
+
+print "What's your city?"
+city = gets.chomp.capitalize!
+city = "Atlanta"
+puts "Your city is #{city}!"
+
+print "What's your state?"
+state = gets.chomp.upcase!
+state = "Georgia"
+puts "Your name is #{Georgia}!"
